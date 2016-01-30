d202d458125d9542cfc1d9f40a9a11b0cec86c68
Dan Schoonmaker <dschoon@gmail.com>
2016-01-30 10:14:59 -0600

    First design

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import SocialShare from '../../lib/index';
 
-let App = React.createClass({
-  render() {
-    return (
-      <div className="example">
-        <h1>social-share</h1>
-        <SocialShare/>
-      </div>
-    );
-  }
+const App = React.createClass({
+    render() {
+        const networks = [{
+            // Twitter
+            src: 'http://bit.ly/1Srk5KZ',
+        }, {
+            // Facebook
+            src: 'http://bit.ly/1PGR74U'
+        }, {
+            // Google+
+            src: 'http://bit.ly/1PLBvTU'
+        }
+        ];
+        return (
+            React.DOM.div({ id: '', className: 'wrapper' },
+                React.DOM.h1({ id: this.props.id, className: this.props.class }, 'Social Share Buttons'),
+                React.createElement(SocialShare, { networks })
+            )
+        );
+    }
 });
 
-React.render(<App/>, document.getElementById('container'));
+ReactDOM.render(React.createElement(App), document.getElementById('container'));
